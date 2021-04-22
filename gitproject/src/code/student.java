828286ed1f21a4d950e6b413d4dc5aec6a01864f
Oshan Nanayakkara <Oshan Nanayakkara@DESKTOP-Q3THJP2.local>
2021-04-22 18:41:52 +0530

    student class initial commit

---
+++ gitproject/src/code/student.java
@@ -0,0 +1,28 @@
+package code;
+
+public class student {
+	
+	private int studentid;
+	private String name;
+	public student(int studentid, String name) {
+		super();
+		this.studentid = studentid;
+		this.name = name;
+	}
+	public int getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(int studentid) {
+		this.studentid = studentid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
