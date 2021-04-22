bddcda4d732afcdec88ca0c54dac42670d5da0a2
Oshan Nanayakkara <Oshan Nanayakkara@DESKTOP-Q3THJP2.local>
2021-04-22 19:08:55 +0530

    teacher initial commit

---
+++ gitproject/src/code/Teacher.java
@@ -0,0 +1,27 @@
+package code;
+
+public class Teacher {
+	
+	private int name;
+	private int salary;
+	public Teacher(int name, int salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+	public int getName() {
+		return name;
+	}
+	public void setName(int name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
